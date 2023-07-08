ae51ac59fecfc177a08751e34155e43afac38a78
anshu <manu22425@gmail.com>
2023-07-09 02:13:21 +0530

    Quotes Generator React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        advice: ''
+    };
+  }
+  componentDidMount(){
+    this.fetchAdvice();
+  }
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice').then((response) => {
+      const advice = response.data.slip.advice;
+      this.setState({
+        advice: advice
+      })
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+  render() {
+    return (
+      <div className='app'>
+        <div className='card'>
+          <h1 className='heading'>{this.state.advice}</h1>
+        <button className='button' onClick={this.fetchAdvice}>
+          <span>GIVE ME A NEW QUOTE!!!</span>
+        </button>
+        </div>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
